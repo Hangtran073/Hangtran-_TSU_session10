277e33c355a38c70cc00c0caffa501b8e1509ddc
Hangtran073 <hangt6465@gmail.com>
2025-03-02 20:12:24 +0900

    upload session10 bài 04

---
+++ ex04.js
@@ -0,0 +1,8 @@
+//khai báo biến width và height
+let width = 20; //độ rộng hình chữ nhật
+let height = 30; //chiều cao hình chữ nhật
+//Tính diện tích hình chữ nhật
+let area = width * height; //
+alert("Diện tích hình chữ nhật là: " + area);
+console.log("Diện tích hình chữ nhật là: " + area);
+document.write("Diện tích hình chữ nhật là: " + area);
