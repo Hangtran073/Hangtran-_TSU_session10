7ca106c819764f8ccd3f7815869bedc3b4932d65
Hangtran073 <hangt6465@gmail.com>
2025-03-02 20:11:51 +0900

    upload session10 bài 03

---
+++ ex03.js
@@ -0,0 +1,8 @@
+//Hiển thị tab console
+console.log( "wellcome to Rikkei Academy");
+//Hiển thị trên màn hình trình duyệt
+document.write("Quyết tâm học lập trình javacript");
+//Hiển thị hộp thoại thông báo 
+alert("Chào mừng bạn đến với học viện Rikkei Academy");
+//Hiển thị hộp thoại xác nhận
+confirm("bạn đã đủ 18 tuổi chưa!");
