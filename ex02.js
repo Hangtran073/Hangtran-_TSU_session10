34c46b08b47bfc03a940add61c4d636ae847bcbc
Hangtran073 <hangt6465@gmail.com>
2025-03-02 20:11:30 +0900

    upload session10 bài 02

---
+++ ex02.js
@@ -0,0 +1,10 @@
+//Khai báo các biến
+let i = 10; //số nguyên 
+let f = 20.5;// số thực
+let b = true; // kiểu logic(boolean)
+let s = "Hà Nội"  //chuỗi(String)
+//Hiển thị ra kết quả màng hình
+document.write("Biến i: " + i + "<br>");
+document.write("Biến f: " + f + "<br>");
+document.write("Biến b: " + b + "<br>");
+document.write("Biến s: " + s + "<br>");
